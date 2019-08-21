4c0f8733b6cf212b777ba97a65460c7235da2bc7
Alexandre Flamant <alexflamant@gmail.com>
2019-08-21 18:26:13 +0100

    added algolia search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,6 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
-
+import {initAutocomplete} from '../plugins/init_autocomplete'
 initMapbox();
+initAutocomplete();
