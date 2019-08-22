c57bf5da24a999a2ea5cbc6975760f2aed8121dc
Ellen Lee <ellen811003@gmail.com>
2019-08-22 11:11:06 +0100

    booking counter and button working now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
-import "../plugins/flatpickr" //Added by AlexFlamant
+// import "../plugins/flatpickr" //Added by AlexFlamant
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import { bookingIncrementer } from '../booking_button';
 
 initMapbox();
+bookingIncrementer();
